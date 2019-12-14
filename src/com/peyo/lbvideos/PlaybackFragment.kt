db31f37fd00f07417fddd6d5b2e4a996ca81ddd7
Peter Yoon <peyo.hd@gmail.com>
2019-12-14 20:29:34 +0900

    apply ExoPlayer MediaSessionConnector to PlaybackFragment

--- src/com/peyo/lbvideos/PlaybackFragment.kt
+++ src/com/peyo/lbvideos/PlaybackFragment.kt
@@ -3,41 +3,71 @@ package com.peyo.lbvideos
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.support.v4.media.session.MediaSessionCompat
 import androidx.leanback.app.VideoSupportFragment
 import androidx.leanback.app.VideoSupportFragmentGlueHost
-import androidx.leanback.media.MediaPlayerAdapter
-import androidx.leanback.media.PlaybackGlue
 import androidx.leanback.media.PlaybackTransportControlGlue
 import androidx.leanback.widget.Action
 import androidx.leanback.widget.ArrayObjectAdapter
 import androidx.leanback.widget.PlaybackControlsRow
 import androidx.navigation.fragment.navArgs
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ext.leanback.LeanbackPlayerAdapter
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import java.lang.Long.max
 import java.lang.Long.min
 
 class PlaybackFragment : VideoSupportFragment() {
     private val args: PlaybackFragmentArgs by navArgs()
 
+    private lateinit var player : SimpleExoPlayer
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaSessionConnector: MediaSessionConnector
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MediaPlayerGlue(requireContext(), MediaPlayerAdapter(activity)).apply {
-            setHost(VideoSupportFragmentGlueHost(this@PlaybackFragment))
-            addPlayerCallback(object : PlaybackGlue.PlayerCallback() {
-                override fun onPreparedStateChanged(glue: PlaybackGlue) {
-                    if (glue.isPrepared) {
-                        //playerGlue.seekProvider = MySeekProvider()
-                        play()
-                    }
-                }
-            })
+
+        player = SimpleExoPlayer.Builder(requireContext()).build()
+        mediaSession = MediaSessionCompat(requireContext(), "LbVideos")
+        mediaSessionConnector = MediaSessionConnector(mediaSession)
+
+        val adapter = LeanbackPlayerAdapter(requireContext(), player, 200)
+
+        MediaPlayerGlue(requireContext(), adapter).apply {
+            host = VideoSupportFragmentGlueHost(this@PlaybackFragment)
+
             title = args.metadata.title
             subtitle = args.metadata.studio
-            playerAdapter.setDataSource(Uri.parse(args.metadata.source))
+
+            val dataSourceFactory = DefaultDataSourceFactory(requireContext(), "LbVideos")
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(Uri.parse(args.metadata.source))
+            player.prepare(mediaSource, true, true)
+            playWhenPrepared()
         }
     }
 
-    private inner class MediaPlayerGlue(context: Context, adapter: MediaPlayerAdapter) :
-            PlaybackTransportControlGlue<MediaPlayerAdapter>(context, adapter) {
+    override fun onResume() {
+        super.onResume()
+        mediaSessionConnector.setPlayer(player)
+        mediaSession.isActive = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaSession.isActive = false
+        mediaSessionConnector.setPlayer(null)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaSession.release()
+    }
+
+    private inner class MediaPlayerGlue(context: Context, adapter: LeanbackPlayerAdapter) :
+            PlaybackTransportControlGlue<LeanbackPlayerAdapter>(context, adapter) {
         private val rewindAction = PlaybackControlsRow.RewindAction(context)
         private val fastForwardAction = PlaybackControlsRow.FastForwardAction(context)
 
@@ -51,9 +81,9 @@ class PlaybackFragment : VideoSupportFragment() {
 
         override fun onActionClicked(action: Action) = when(action){
             rewindAction -> playerAdapter.seekTo(
-                    max(0, playerAdapter.currentPosition - 5000))
+                    max(0, playerAdapter.currentPosition - 10000))
             fastForwardAction -> playerAdapter.seekTo(
-                    min(playerAdapter.duration, playerAdapter.currentPosition + 5000))
+                    min(playerAdapter.duration, playerAdapter.currentPosition + 10000))
             else -> super.onActionClicked(action)
         }
     }
